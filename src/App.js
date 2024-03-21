cb2131c15731a943b7f8f82addf02bdd05f268c5
STvignesh <stvignesh5@gmail.com>
2024-03-21 15:11:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/pattern-divider-desktop.svg";
+import logo1 from "./images/pattern-divider-mobile.svg";
+import diceLogo from "./images/icon-dice.svg";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState();
+  const [id, setId] = useState();
+  const [error, setError] = useState("");
+  async function getAdvice() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const { slip } = await response.json();
+      console.log(slip);
+      console.log(slip.advice);
+      setAdvice(`"${slip.advice}"`);
+      setId(slip.id);
+    } catch (error) {
+      setError(error);
+    }
+  }
+  useEffect(() => {
+    getAdvice();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="outer-cont">
+        <div className="advice-container-box">
+          <h2 className="adviceNumber">advice #{id}</h2>
+          <p className="advice">{error === "" ? advice : error}</p>
+          <div
+            className="
+          logo-cont"
+          >
+            <img
+              src={logo}
+              alt="seperator-logo"
+              className="logo-seperator-desktop"
+            ></img>
+            <img
+              src={logo1}
+              alt="seperator-logo"
+              className="logo-seperator-mobile"
+            ></img>
+            <button className="btn" onClick={getAdvice}>
+              <img
+                src={diceLogo}
+                alt="diceLogo"
+                className="dice-logo"
+                onClick={getAdvice}
+              ></img>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
